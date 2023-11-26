fc013a9391b27622756ed88e1f379ddf4b8d20f8
Muhammad Yasir <145558820+realmyasir@users.noreply.github.com>
2023-11-26 12:32:30 +0500

    Add files via upload

---
+++ Area Of circle In Python using user Input.py
@@ -0,0 +1,42 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Nov 21 10:29:14 2023
+
+@author: Web 123
+"""
+
+# ask the Are of your circle  and print he area
+# radius=float(input('Enter your Area'))
+# pi=3.14159
+# area=pi*radius**2
+# print('You Entered ',radius,' The Area of Circle is',area)
+
+
+#convert Farhrenheit to celsius
+
+# far=float(input('please enter Temperature in Farhrenheit  >>>'))
+# cel= (far-32)*5/9
+# print(far,'Farhrenheit in Celsius is',cel)
+
+# obtain the sum of two integer
+
+# num_1 = int(input('Enter The First Number'))
+# num_2 = int(input('Enter your second Number'))
+# print('The Sum of  '+str(num_1)+' and '+str(num_2) +' is ' +str(num_1+num_2))
+
+
+# obtain the product of two integer
+
+# num_1 = int(input('Enter The First Number>>'))
+# num_2 = int(input('Enter your second Number>>'))
+
+
+# print('The Multiply of  '+str(num_1)+' and '+str(num_2) +' is ' +str(num_1*num_2))
+
+
+# total_slice = 4*4
+# number_of_pizza= (total_slice+5)//6
+# slice_left=number_of_pizza*6-total_slice
+# print('Number Of Pizzas Required is  ',number_of_pizza,'there Will be',slice_left,'remaining Slices')
+
+
